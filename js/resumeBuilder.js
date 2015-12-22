be067fb8ab3d1382209f65dcfe4fe4799874ef41
Adrian Heydecker <heydecker@users.noreply.github.com>
2015-12-22 19:03:14 +0100

    opened up sublime project, did some first js tweaks https://www.udacity.com/course/viewer#!/c-ud804/l-1930528550/e-1919128734/m-1951108681

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,21 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+/*function appendName(){	
+	$("#main").append("Adrian Heydecker");
+}
+*/
+
+var appendToMain = function(myString){  
+  $("#main").append("<p>"+myString+"</p>");
+};
+
+appendToMain("Adrian Heydecker");
+
+var awesomeThoughts = "I am Adrian and I am super AWESOME again";
+
+var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+
+appendToMain(funThoughts);
+
