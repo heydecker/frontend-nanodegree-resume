b90bbb3a550c2ad1bb9f47e39c5a0ca911c29ecc
Adrian Heydecker <heydecker@users.noreply.github.com>
2015-12-23 12:49:40 +0100

    https://www.udacity.com/course/viewer#!/c-ud804/l-1930528550/e-2885918558/m-2883278539

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,11 +2,14 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-/*function appendName(){	
-	$("#main").append("Adrian Heydecker");
-}
+/*
+var appendTo = function(myElement,myHTML){  
+//  myElement.append("<p>"+myString+"</p>");
+  $(myElement).append(myHTML);
+};
 */
 
+/*
 var appendToMain = function(myString){  
   $("#main").append("<p>"+myString+"</p>");
 };
@@ -14,8 +17,26 @@ var appendToMain = function(myString){
 appendToMain("Adrian Heydecker");
 
 var awesomeThoughts = "I am Adrian and I am super AWESOME again";
-
 var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
 
 appendToMain(funThoughts);
+*/
+var name = "Adrian Heydecker";
+var role = "UX Architect";
+
+var formattedName = HTMLheaderName.replace("%data%",name);
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var myInitialString = "audacity";
+var myFinalString1 = myInitialString.replace("a","");
+var myFinalString2 = myInitialString.slice(1);
+
+console.log(myFinalString1);
+console.log(myFinalString2);
+
+
+
 
